eae4158804b0fcd9068c2d77cb6876468244745c
Rebecca <rxs6737@rit.edu>
2019-08-08 22:36:45 -0700

    made nav into buttons, added state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'website';
   linkNames = ["welcome", "about", "experience", "projects", "contact"];
+  state = "test";
+
+  onUpdateState(state: string) {
+      this.state = state;
+  }
 }
